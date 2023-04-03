2c77f05720e069b74b813f934ccc34cd7cc7e7f7
CrimsonCoder42 <nostro37@gmail.com>
2023-04-03 09:15:14 -0400

    Chapter 8

--- chapter7/build/js/main.js
+++ chapter7/build/js/main.js
@@ -1,5 +1,8 @@
 "use strict";
 // Index Signatures in TypeScript
+// interface TransactionObj {
+//     readonly [index: string]: number
+// }
 const todaysTransactions = {
     Pizza: -10,
     Books: -5,
@@ -15,4 +18,27 @@ const todaysNet = (transactions) => {
     }
     return total;
 };
-console.log(todaysNet(todaysTransactions));
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+//console.log(student.test)
+for (const key in student) {
+    console.log(`${key}: ${student[key]} `);
+}
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const lofStudentKey = (student, key) => {
+    console.log(`Student ${key} is ${student[key]}`);
+};
+lofStudentKey(student, 'GPA');
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+};
+for (const revenue in monthlyIncomes) {
+    console.log(`${revenue}: ${monthlyIncomes[revenue]}`);
+}
--- chapter7/src/main.ts
+++ chapter7/src/main.ts
@@ -1,14 +1,15 @@
 // Index Signatures in TypeScript
 
-// interface TransactionObj {
-//     Pizza: number,
-//     Books: number,
-//     Job: number
-// }
-
 interface TransactionObj {
-    [index: string]: number
+    readonly [index: string]: number
+    Pizza: number,
+    Books: number,
+    Job: number
 }
+
+// interface TransactionObj {
+//     readonly [index: string]: number
+// }
 
 const todaysTransactions: TransactionObj = {
     Pizza: -10,
@@ -29,4 +30,72 @@ const todaysNet = (transactions: TransactionObj): number => {
     return total
 }
 
-console.log(todaysNet(todaysTransactions))
+////////////////////////////////////////////////////////////////////////
+
+interface Student {
+    [key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+}
+
+
+const student: Student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+}
+
+
+//console.log(student.test)
+
+
+for (const key in student) {
+    console.log(`${key}: ${student[key as keyof Student]} `)
+}
+
+Object.keys(student).map(key => {
+    console.log(student[key as keyof typeof student])
+})
+
+const lofStudentKey = (student: Student, key: keyof Student): void => {
+    console.log(`Student ${key} is ${student[key]}`)
+}
+
+lofStudentKey(student, 'GPA')
+
+
+
+////////////////////////////////////////////
+
+
+
+// interface Incomes {
+//     [key: string]: number
+// }
+
+type Streams = `salary` | 'bonus' | 'sidehustle'
+
+
+type Incomes = Record<Streams, number | string>
+
+const monthlyIncomes: Incomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+}
+
+for (const revenue in monthlyIncomes) {
+    console.log(`${revenue}: ${monthlyIncomes[revenue as keyof Incomes]}`)
+}
+
+
+
+
+
+
+
+
+
+
+
